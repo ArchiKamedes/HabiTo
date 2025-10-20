343cc00acf59abf95d20d9601f4dfb0d2d3b2224
QKama <136479@student.uni.opole.pl>
2025-10-20 11:26:03 +0200

    test expo go

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React, { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { View, ActivityIndicator, StyleSheet } from 'react-native';
+
+import { auth } from './src/firebaseConfig'; // Ścieżka do skopiowanego pliku
+import { onAuthStateChanged, signInAnonymously } from 'firebase/auth';
+
+import RootStack from './src/navigation/RootStack';
 
 export default function App() {
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+      } else {
+        signInAnonymously(auth).catch((error) => {
+          console.error("Błąd logowania anonimowego:", error);
+        });
+      }
+      setIsLoading(false);
+    });
+    return () => unsubscribe();
+  }, []); 
+
+  if (isLoading) {
+    return (
+      <View style={styles.loader}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  loader: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
+    alignItems: 'center',
+  }
 });
